440ce9f465b146192fa06be5c006d48edccd3e0d
bereg <git@a-bereg.ru>
2021-05-04 18:37:26 +0300

    ex 7 Cat added

--- ex_07/03_man_ans_cat_my.py
+++ ex_07/03_man_ans_cat_my.py
@@ -34,6 +34,7 @@ class Man:
         self.fullness = 50
         self.food = 50
         self.house = None
+        self.cat = None
 
     def __str__(self):
         return 'Я {}, сытость - {}, еды - {}, денег - {}.'.format(self.name, self.fullness, self.food, self.money)
@@ -57,11 +58,11 @@ class Man:
     def work(self):
         if self.house is None:
             cprint('{} пошел на работу.' .format(self.name), color='magenta')
-            self.money += 50
+            self.money += 150
             self.fullness -= 20
         else:
             cprint('{} пошел на работу.' .format(self.name), color='magenta')
-            self.house.money += 50
+            self.house.money += 150
             self.fullness -= 20
 
     def play_dota(self):
@@ -84,6 +85,19 @@ class Man:
             else:
                 cprint('В доме мало денег - {} ' .format(self.house.money), color='red')
 
+    def clean(self):
+        cprint('{} убрался в доме.' .format(self.name), color='white')
+        self.house.trash -= 100
+        self.fullness -= 20
+
+    def bay_cats_food(self):
+        if self.house.money >= 50:
+            cprint('{} сходил в магазин за едой для кота.'.format(self.name), color='blue')
+            self.house.money -= 50
+            self.house.cat_food += 50
+        else:
+            cprint('В доме мало денег - {}, кот останется голодным '.format(self.house.money), color='red')
+
     def act(self):
         if self.fullness <= 0:
             cprint('{} умер!!!' .format(self.name), color='red')
@@ -99,6 +113,10 @@ class Man:
             self.shopping()
         elif self.house is not None and self.house.food < 30:
             self.shopping()
+        elif self.house.cat_food <= 30:
+            self.bay_cats_food()
+        elif self.house.trash >= 100:
+            self.clean()
         elif dice == 1:
             self.work()
         elif dice == 2:
@@ -115,32 +133,90 @@ class Man:
         self.food = 0
         cprint('{} заселился в дом!!!' .format(self.name), color='cyan')
 
+    def take_pet(self, cat):
+        self.cat = cat
+        self.cat.house = self.house
+        cprint('{} завел кота {}'.format(self.name, self.cat.name))
 
-class House():
+
+class House:
     def __init__(self):
         self.food = 10
         self.money = 50
+        self.trash = 0
+        self.cat_food = 50
 
     def __str__(self):
-        return 'В доме есть: еды - {}, денег - {}.' .format(self.food, self.money)
+        return 'В доме есть: еды - {}, денег - {}, Wiskas - {}, мусор - {}.' .format(
+            self.food, self.money, self.cat_food, self.trash)
+
+
+class Cat:
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 20
+        self.house = None
+
+    def __str__(self):
+        return 'Кот {}, сытость {}.' .format(self.name, self.fullness)
+
+    def sleep(self):
+        cprint('Кот {} спит.' .format(self.name), color='grey')
+        self.fullness -= 10
+
+    def eat(self):
+        cprint('Кот {} поел.' .format(self.name), color='grey')
+        self.house.cat_food -= 10
+        self.fullness += 20
+
+    def make_trash(self):
+        cprint('Кот {} дерет обои.'.format(self.name), color='grey')
+        self.house.trash += 5
+        self.fullness -= 10
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint('Кот {} - умер' .format(self.name), color='red')
+            return
+        dice = randint(2, 3)
+        if self.fullness <= 20:
+            self.eat()
+        elif dice == 2:
+            self.sleep()
+        elif dice == 3:
+            self.make_trash()
 
 
 sweet_home = House()
-vasya = Man(name='Вася')
-vasya.go_into_house(house=sweet_home)
+citizens = []
+names_for_cit = ['Вася', 'Энди', 'Фёдор', 'Таня', 'Света', 'Миша', 'Алексей', 'Саша']
+names_for_cat = ['Том', 'Гав', 'Мурзик', 'Бармалей', 'Мурка']
+my_pet = Cat(names_for_cat[randint(0, 4)])         #создаем кота
+my_pet2 = Cat(names_for_cat[randint(0, 4)])
 
-fedor = Man(name='Фёдор')
-fedor.go_into_house(house=sweet_home)
+# Создаем людей
+for i in range(1):
+    citizens.append(Man(names_for_cit[randint(0, 7)]))
 
-for day in range(1, 360):
+# Заселяем людей в дом
+for people in citizens:
+    people.go_into_house(sweet_home)
+
+citizens[0].take_pet(my_pet)
+citizens[0].take_pet(my_pet2)
+
+for day in range(1, 365):
     cprint('=================== День {} ===================' .format(day), color='yellow')
-    vasya.act()
-    fedor.act()
+    for people in citizens:
+        people.act()
+    my_pet.act()
+    my_pet2.act()
     print(sweet_home)
     cprint('-----------------------------------------------', color='yellow')
-    print(vasya)
-    print(fedor)
-
+    for people in citizens:
+        print(people)
+    print(my_pet)
+    print(my_pet2)
 
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.
