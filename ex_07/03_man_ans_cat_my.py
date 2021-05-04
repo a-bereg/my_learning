861c80a65697e70002538c370a408700d3b3b0d1
bereg <git@a-bereg.ru>
2021-05-04 16:00:03 +0300

    ex 7 House added

--- ex_07/03_man_ans_cat_my.py
+++ ex_07/03_man_ans_cat_my.py
@@ -26,40 +26,63 @@ from termcolor import cprint
 
 # Человеку и коту надо вместе прожить 365 дней.
 
+
 class Man:
     def __init__(self, name):
         self.name = name
         self.money = 10
         self.fullness = 50
         self.food = 50
+        self.house = None
 
     def __str__(self):
         return 'Я {}, сытость - {}, еды - {}, денег - {}.'.format(self.name, self.fullness, self.food, self.money)
 
     def eat(self):
-        if self.food > 10:
-            cprint('{} поел. :)' .format(self.name), color='yellow')
-            self.fullness += 10
-            self.food -= 10
+        if self.house is None:
+            if self.food >= 10:
+                cprint('{} поел. :)' .format(self.name), color='yellow')
+                self.fullness += 10
+                self.food -= 10
+            else:
+                cprint('{} нет еды. :('.format(self.name), color='red')
         else:
-            cprint('{} нет еды. :('.format(self.name), color='red')
+            if self.house.food >= 10:
+                cprint('{} поел. :)'.format(self.name), color='yellow')
+                self.fullness += 10
+                self.house.food -= 10
+            else:
+                cprint('{} нет еды. :('.format(self.name), color='red')
 
     def work(self):
-        cprint('{} пошел на работу.' .format(self.name), color='magenta')
-        self.money += 50
-        self.fullness -= 20
+        if self.house is None:
+            cprint('{} пошел на работу.' .format(self.name), color='magenta')
+            self.money += 50
+            self.fullness -= 20
+        else:
+            cprint('{} пошел на работу.' .format(self.name), color='magenta')
+            self.house.money += 50
+            self.fullness -= 20
 
-    def play_DOTA(self):
+    def play_dota(self):
         cprint('{} играет в DOT-у. :)' .format(self.name), color='green')
         self.fullness -= 10
 
     def shopping(self):
-        if self.money >= 50:
-            cprint('{} сходил в магазин за едой.' .format(self.name), color='blue')
-            self.money -= 50
-            self.food += 50
+        if self.house is None:
+            if self.money >= 50:
+                cprint('{} сходил в магазин за едой.' .format(self.name), color='blue')
+                self.money -= 50
+                self.food += 50
+            else:
+                cprint('{} нет денег' .format(self.name), color='red')
         else:
-            cprint('{} нет денег' .format(self.name), color='red')
+            if self.house.money >= 50:
+                cprint('{} сходил в магазин за едой.' .format(self.name), color='blue')
+                self.house.money -= 50
+                self.house.food += 50
+            else:
+                cprint('В доме мало денег - {} ' .format(self.house.money), color='red')
 
     def act(self):
         if self.fullness <= 0:
@@ -68,23 +91,55 @@ class Man:
         dice = randint(1, 6)
         if self.fullness <= 20:
             self.eat()
-        elif self.food < 30:
-            self.shopping()
-        elif self.money < 20:
+        elif self.house is None and self.money <= 20:
             self.work()
+        elif self.house is not None and self.house.money <= 20:
+            self.work()
+        elif self.house is None and self.food < 30:
+            self.shopping()
+        elif self.house is not None and self.house.food < 30:
+            self.shopping()
         elif dice == 1:
             self.work()
         elif dice == 2:
             self.eat()
         else:
-            self.play_DOTA()
+            self.play_dota()
+
+    def go_into_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        self.house.money += self.money
+        self.money = 0
+        self.house.food += self.food
+        self.food = 0
+        cprint('{} заселился в дом!!!' .format(self.name), color='cyan')
 
 
+class House():
+    def __init__(self):
+        self.food = 10
+        self.money = 50
+
+    def __str__(self):
+        return 'В доме есть: еды - {}, денег - {}.' .format(self.food, self.money)
+
+
+sweet_home = House()
 vasya = Man(name='Вася')
-for day in range(1, 366):
+vasya.go_into_house(house=sweet_home)
+
+fedor = Man(name='Фёдор')
+fedor.go_into_house(house=sweet_home)
+
+for day in range(1, 360):
     cprint('=================== День {} ===================' .format(day), color='yellow')
     vasya.act()
+    fedor.act()
+    print(sweet_home)
+    cprint('-----------------------------------------------', color='yellow')
     print(vasya)
+    print(fedor)
 
 
 # Усложненное задание (делать по желанию)
